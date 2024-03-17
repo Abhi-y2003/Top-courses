29709bd7bcbde8c411bffd8dd81388250f40c35d
Abhi-y2003 <22633001@gkv.ac.ingit config --global user.name Abhi-y2003>
2024-03-18 01:23:32 +0530

    Logic Done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Cards from './componensts/Cards.js'
 import {apiUrl ,filterData} from './data.js'
 import {useEffect } from 'react'
 import { toast } from 'react-toastify'
+import Spinner from './componensts/Spinner.js'
+
+
 const App = () => {
 
 
   const[courses, setCourses] = useState(null);
+
+  const[loading, setLoading] = useState(true);
     
-  useEffect( ()=>{
-    const FetchData = async() =>{
+ 
+    async function FetchData(){
+      setLoading(true);
       try {
         const res = await fetch(apiUrl);
         const output = await res.json();
@@ -20,18 +26,31 @@ const App = () => {
       } catch (error) {
         toast.error("something went wrong");
       }
-    }
-    FetchData();
-  },[])
+      setLoading(false);
+    };
+
+    useEffect( ()=>{
+      FetchData();
+    },[])
   
   return (
     <div>
-      App
+      <div>
       <Navbar/>
+      </div>
+      
 
+      <div>
       <Filter filterData={filterData}/>
+      </div>
+      
 
-      <Cards courses={cources}/>
+
+      <div>
+        {loading? <Spinner/>  : <Cards courses={courses}/>}
+      
+      </div>
+      
     </div>
   )
 }
--- src/componensts/Cards.js
+++ src/componensts/Cards.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import Card from './Card.js'
 
 const Cards = ({courses}) => {
-  return (
+  
+  
+  
+  //sara courses ka data bikhra hua hai i mean development ka alag data hai similarly sabka aise he hai
+  // but we want k sara data ek sath ho  
+  const getCourses =() =>{
+    let allCourses=[]; 
 
-    //sara courses ka data bikhra hua hai i mean development ka alag data hai similarly sabka aise he hai
-    // but we want k sara data ek sath ho  
-    
-    
+      Object.values(courses).forEach(array => {    //object.value karke hame first array ko phad skte devlopment lifestyle etc now foreach laga k we are the inside content of each array.  
+        array.forEach(course=>{                    //another foreach loop jo ham devlopment lifestyle har ek array k under aa gye the ham one step deep or aa gye  data-->development --> objects  1st for each loop se devlopment me 2nd wale se apne object me 
+          allCourses.push(course)                             //aab ham har ek object ko push krdenge ek naye array me jaha sare courses honge bina kisi dikkat k
+        })
+
+      });
+      return allCourses;
+  }
+  return (
     <div>
-    {courses.map(course)}
+    {getCourses().map((course)=>{
+      return <Card key={course.id} course={course}/>
+    })}
 
     </div>
   )
